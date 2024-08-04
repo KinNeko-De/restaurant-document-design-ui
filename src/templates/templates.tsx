75fa20efd6183369c1373e4d2b7a674106a919d1
Nils Diekmann <github@kinneko.de>
2024-08-04 10:11:37 +0200

    - add tags

--- src/templates/templates.tsx
+++ src/templates/templates.tsx
@@ -15,6 +15,7 @@ interface Item {
   language: TemplateLanguage;
   favourite: boolean;
   lastModified: Date;
+  tags?: string[]
 }
 
 enum TemplateLanguage {
@@ -59,9 +60,9 @@ const getLanguageColor = (value: TemplateLanguage): string => {
 
 const Templates: React.FC = () => {
   const items: Item[] = [
-    { name: 'Item 1', description: 'This is the first item.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2024-08-03T19:00:00') },
-    { name: 'Item 2', description: 'This is the second item.', language: TemplateLanguage.Word, favourite: false, lastModified: new Date('2023-10-02T14:30:00')},
-    { name: 'Item 3', description: 'This is the second item.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2023-10-03T16:45:00') },
+    { name: 'Item 1', description: 'This is the first item.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2024-08-03T19:00:00'), tags: ['tag 1', 'tag 2'] },
+    { name: 'Item 2', description: 'This is the second item.', language: TemplateLanguage.Word, favourite: false, lastModified: new Date('2023-10-02T14:30:00') },
+    { name: 'Item 3', description: 'This is the second item.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2023-10-03T16:45:00'), tags: ['tag 3'] },
     { name: 'Item 4', description: 'This is the second item.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2023-07-04T18:00:00') },
   ];
 
@@ -69,7 +70,8 @@ const Templates: React.FC = () => {
     <Grid container rowSpacing={'1rem'} columnSpacing={{ xs: '1rem' }} sx={{ m: '0.5rem' }}>
       {items.map((item) => (
         <Grid xs={12} sm={6} md={4} key={item.name}>
-          <Card>
+          <Card sx={{ height: '100%' }}>
+          <Box display="flex" flexDirection="column" height="100%">
             <CardContent>
               <Typography variant="h5" component="div">
                 {item.name}
@@ -77,28 +79,52 @@ const Templates: React.FC = () => {
               <Typography variant="body2" color="text.secondary">
                 {item.description}
               </Typography>
+              {item.tags && item.tags.length > 0 && (
+              <Box display="flex" flexWrap="wrap" gap={1} mt={1}>
+                {item.tags.map((tag, index) => (
+                  <Box
+                    key={index}
+                    sx={{
+                      display: 'inline-flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      padding: '0.25rem 0.5rem',
+                      borderRadius: '16px',
+                      backgroundColor: '#e0e0e0',
+                      color: '#424242',
+                      fontSize: '0.875rem',
+                    }}
+                  >
+                    {tag}
+                  </Box>
+                ))}
+              </Box>
+            )}
             </CardContent>
+            <Box mt="auto">
             <CardActions style={{ display: 'flex', justifyContent: 'space-between' }}>
               <Box display="flex" alignItems="center" gap={2}>
-              <Box display="flex" alignItems="center" gap={0.5}>
-    <Box
-      sx={{
-        width: 10,
-        height: 10,
-        borderRadius: '50%',
-        backgroundColor: getLanguageColor(item.language),
-      }}
-    />
-    <Typography variant="body2" color="text.secondary">
-      {TemplateLanguage[item.language]}
-    </Typography>
-  </Box>
+                <Box display="flex" alignItems="center" gap={0.5}>
+                  <Box
+                    sx={{
+                      width: 10,
+                      height: 10,
+                      borderRadius: '50%',
+                      backgroundColor: getLanguageColor(item.language),
+                    }}
+                  />
+                  <Typography variant="body2" color="text.secondary">
+                    {TemplateLanguage[item.language]}
+                  </Typography>
+                </Box>
                 <Typography variant="body2" color="text.secondary">
                   {formatDate2(item.lastModified)}
                 </Typography>
               </Box>
               <FavoriteButton />
             </CardActions>
+            </Box>
+            </Box>
           </Card>
         </Grid>
       ))}
