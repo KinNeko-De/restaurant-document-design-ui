b3b7ca0bd632ff76ae33bb66d7399c3604991702
Nils Diekmann <github@kinneko.de>
2024-08-06 00:53:54 +0200

    - simulate api call - display loading animation

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Templates from "./design-document/templates";
 import PageNotFound from "./pageNotFound";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Templates/>,
+    loader: () => import("./design-document/templates"),
     ErrorBoundary: PageNotFound
   }
 ]);
