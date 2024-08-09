1dfd5b141964955fabadbe9c55375e2555a3603a
Nils Diekmann <github@kinneko.de>
2024-08-10 01:01:45 +0200

    - add describe

--- src/design-document/templates.test.tsx
+++ src/design-document/templates.test.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import Templates from './templates';
 
-test('loading animation is shown', async () => {
-  render(<Templates />);
+describe('Templates', () => {
+  test('loading animation is shown while loading', async () => {
+    render(<Templates />);
 
-  expect(screen.getByTestId('template-loading')).toBeInTheDocument();
-});
+    expect(screen.getByTestId('template-loading')).toBeInTheDocument();
+  });
 
-test('templates are display after loaded', async () => {
-  render(<Templates />);
+  test('templates are displayed after loaded', async () => {
+    render(<Templates />);
 
-  await waitFor(() => {
-    const templates = screen.getAllByTestId('template-loaded');
-    expect(templates).toHaveLength(4);
-  }, { timeout: 3000 });
+    await waitFor(() => {
+      const templates = screen.getAllByTestId('template-loaded');
+      expect(templates).toHaveLength(4);
+    }, { timeout: 3000 });
+  });
 });
