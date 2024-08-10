49138e7df2222d0ce96dda2b8de92c7a45d8e35d
Nils Diekmann <github@kinneko.de>
2024-08-10 09:42:20 +0200

    - add coverage - add codecov

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Templates from "./design-document/templates";
 import PageNotFound from "./pageNotFound";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
