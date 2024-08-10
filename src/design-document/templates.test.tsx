7b5201690d7d3665c66bd2d274a59c393ed49b23
Nils Diekmann <github@kinneko.de>
2024-08-10 18:06:27 +0200

    - made test more expliciitly typed - extract test factory method

--- src/design-document/templates.test.tsx
+++ src/design-document/templates.test.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Template, TemplateGateway, TemplateLanguage } from './domain';
 import Templates from './templates';
-import { TemplateLanguage } from './domain';
-import '@testing-library/jest-dom'
 import { TEST_IDS } from './testIds';
-
-const mockGateway = {
-  fetchTemplates: jest.fn(),
-  fetchTemplate: jest.fn(),
-};
-
-const Sut: React.FC = () => {
-  return (
-    <Templates 
-      fetchTemplates={mockGateway.fetchTemplates} 
-      fetchTemplate={mockGateway.fetchTemplate} 
-    />
-  );
-};
 
 describe('Templates', () => {
   beforeEach(() => {
@@ -25,7 +10,7 @@ describe('Templates', () => {
   });
 
   test('loading animation is shown while loading', async () => {
-    mockGateway.fetchTemplates.mockImplementationOnce(() => {
+    (mockGateway.fetchTemplates as jest.Mock).mockImplementationOnce(() => {
       return new Promise((resolve) => {
         setTimeout(() => {
           resolve([]);
@@ -39,19 +24,38 @@ describe('Templates', () => {
   });
 
   test('templates are displayed after loaded', async () => {
-    const mockTemplates = [
+    const mockTemplates: Template[] = [
       { name: 'Template 1', description: 'This is the first template.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2024-08-03T19:00:00'), tags: ['tag 1', 'tag 2'], status: 'Active' },
       { name: 'Template 2', description: 'This is the second template.', language: TemplateLanguage.Word, favourite: false, lastModified: new Date('2023-10-02T14:30:00'), status: 'Draft' },
       { name: 'Template 3', description: 'This is the second template.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2023-10-03T16:45:00'), tags: ['tag 3'], status: 'Draft' },
       { name: 'Template 4', description: 'This is the second template.', language: TemplateLanguage.LuaLaTex, favourite: false, lastModified: new Date('2023-07-04T18:00:00'), status: 'Draft' },
     ];
-    mockGateway.fetchTemplates.mockResolvedValueOnce(mockTemplates);
+    setupFetchTemplates(mockTemplates);
 
     render(<Sut/>);
 
     await waitForElementToBeRemoved(() => screen.queryByTestId(TEST_IDS.TEMPLATE_LOADING));
-
     const templates = screen.getAllByTestId(TEST_IDS.TEMPLATE_LOADED);
     expect(templates).toHaveLength(4);
   });
 });
+
+const mockGateway: TemplateGateway = {
+  fetchTemplates: jest.fn() as jest.MockedFunction<TemplateGateway['fetchTemplates']>,
+  fetchTemplate: jest.fn() as jest.MockedFunction<TemplateGateway['fetchTemplate']>,
+};
+
+const Sut: React.FC = () => {
+  return (
+    <Templates 
+      fetchTemplates={mockGateway.fetchTemplates} 
+      fetchTemplate={mockGateway.fetchTemplate} 
+    />
+  );
+};
+
+function setupFetchTemplates(templates: Template[]) {
+  (mockGateway.fetchTemplates as jest.Mock).mockImplementationOnce(() => {
+    return templates;
+  });
+}
